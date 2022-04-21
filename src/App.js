9d8c5eada4613c66c5758a020360796e1d34168e
Student <owen.kiteck@stu.barren.kyschools.us>
2022-04-21 12:25:36 -0500

    finished bonus

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 import McuShows from "./mcu-shows/McuShows";
 
+import {useEffect, useState} from "react"
+
+const mcuCharacters = [
+  'Wanda Maximoff',
+  'Vision',
+  'Falcon',
+  'Winter Soldier',
+  'Loki',
+  'The Watcher',
+  'Hawkeye',
+  'Kate Bishop',
+  'Moon Knight',
+  'Ms. Marvel'
+];
+
 const releaseDates = {
   wandaVision: 'Jan 2021',
   falconWinter: 'Mar 2021',
@@ -11,10 +26,35 @@ const releaseDates = {
 };
 
 function App() {
+
+  const [index, setIndex] = useState(0)
+  const [num, setNum] = useState(0)
+
+  // The function (1st argument) passed into the useEffect function/hook will run every time the state value ("num" in this example) that is passed into the array (2nd argument) is updated
+  useEffect(() => {
+    alert(`The Next MCU Character has been displayed`)
+  }, [num])
+
+  function randomChar() {
+    setIndex(Math.floor(Math.random() * mcuCharacters.length))
+  }
+
+  function nextChar() {
+    if (num === mcuCharacters.length - 1){
+      setNum(0)
+    } else {
+      setNum(num + 1)
+    }
+  }
+
+
+
   return (
     <div>
       <h1>FINAL REACT EXERCISE</h1>
-      <McuShows dates={releaseDates}/>
+      <McuShows dates={releaseDates} randomChar={randomChar} nextChar={nextChar}/>
+      <h1>TRandom MCU Char: {mcuCharacters[index]}</h1>
+      <h1>TRandom MCU Char: {mcuCharacters[num]}</h1>
     </div>
   );
 }
--- src/mcu-shows/McuShows.js
+++ src/mcu-shows/McuShows.js
@@ -1,9 +1,10 @@
-import "./McuShows"
+import "./McuShows.css"
 
-function McuShows({dates}) {
+function McuShows({dates, randomChar, nextChar}){
+
     return (
         <div className="McuShows">
-            <h1>Disney+</h1>
+            <h1 style={{fontFamily: 'Georgia', textDecoration: 'underline'}}>Disney+</h1>
             <h2>MCU Show Release Dates</h2>
             <p>WandaVision: {dates.wandaVision}</p>
             <p>The Falcon and the Winter: {dates.falconWinter}</p>
@@ -12,6 +13,9 @@ function McuShows({dates}) {
             <p>Hawkeye: {dates.hawkeye}</p>
             <p>Moon Knight: {dates.moonKnight}</p>
             <p>Ms. Marvel: {dates.msMarvel}</p>
+
+            <button onClick={randomChar}>RANDOM MCU CHARACTER</button>
+            <button onClick={nextChar}>NEXT MCU CHARACTER</button>
         </div>
     )
 }
